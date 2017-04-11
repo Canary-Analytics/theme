9e6701b1c642e6665d4b6ad155ad217e081ac2ff
Samuel Ramos <alu0100815146@ull.edu.es>
2017-04-11 18:19:34 +0100

    Mejorando el sistema de vistas

--- Canary_/app.js
+++ Canary_/app.js
@@ -6,13 +6,11 @@
     const express = require('express');
     const app = express();
     const path = require('path');
-    const expressLayouts = require('express-ejs-layouts');
 
     app.set('port', (process.env.PORT || 8080));
 
     app.set('views', path.join(__dirname, 'views'));
     app.set('view engine', 'ejs');
-    app.use(expressLayouts);
 
     app.use(express.static(includesHtml));
 
